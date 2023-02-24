e9dfa2ebaeaad3de6d999323aea5e123b95b8963
stephanniewolff <ste.wolff91@gmail.com>
2023-02-24 14:17:13 -0300

    forca

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LearnView from '../views/LearnView.vue'
 import CalculatorView from '../views/CalculatorView.vue'
+import ForcaView from '../views/ForcaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,14 +23,21 @@ const router = createRouter({
       component: CalculatorView
     },
     {
+      path:'/forca',
+      name:'forca',
+      component: ForcaView
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+      
     }
   ]
 })
+
 
 export default router
