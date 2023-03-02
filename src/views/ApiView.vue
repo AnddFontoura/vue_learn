9c1e36d3b5befad2306e727a48faaf4b7da82e39
André Fontoura <45948615+AnddFontoura@users.noreply.github.com>
2023-03-02 17:42:44 -0300

    Axios

--- src/views/ApiView.vue
+++ src/views/ApiView.vue
@@ -1,6 +1,31 @@
+<script>
+import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        pokemon: null,
+        info: null
+      }
+    },
+    mounted() {
+        axios.get('https://pokeapi.co/api/v2/pokemon/1')
+        .then( response => (this.info = response))
+    },
+    methods: {
+      getPokemon() {
+        axios.get('https://pokeapi.co/api/v2/pokemon/2')
+        .then( response => (this.info = response))
+      }
+    }
+  }
+</script>
+
 <template>
-    <div class="about">
-      <h1>This is an about page</h1>
+    <div class="api-reflex">
+      <h1> Teste </h1>
+      <button @click="getPokemon()"> Get new pokemon </button>
+      {{ info }}
     </div>
   </template>
   
@@ -8,9 +33,13 @@
   @media (min-width: 1024px) {
     .about {
       min-height: 100vh;
-      display: flex;
       align-items: center;
     }
+  }
+
+  .api-reflex {
+    border: 1px solid black;
+    padding: 10px;
   }
   </style>
   
